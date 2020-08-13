8181ad63bb6918ffc0c51abe5a8e75216a66697f
Dorothy Punderson <dorothypunderson@dorothys-air.attlocal.net>
2020-08-13 08:17:29 -0700

    cleaned script

--- LandCover/LULC/NLCD/NLCD_script_2001to2016.js
+++ LandCover/LULC/NLCD/NLCD_script_2001to2016.js
@@ -1,13 +1,22 @@
+//Land use/Land cover extraction script for SSI Climate Change Project
+//Created May 2020
+//Last edited 08/13/2020
+//Authors: DP, WS
+
+//Goal: Get % cover of 5 land classes in Deer Creek site catchments, 2001 and 2016
+
+///////Part 1. Data prep///////
+
 var roi = DC_watershed
 Map.centerObject(roi, 11);
 
-//Get NLCD image from 2001
+//Step 1. Get NLCD image from 2001
 var DC_2001 = ee.ImageCollection("USGS/NLCD")
 .select('landcover')
 .filterDate('2001-01-01')
 .filter(ee.Filter.bounds(roi));
 
-//Get NLCD image from 2016
+//Step 2. Get NLCD image from 2016
 var DC_2016 = ee.ImageCollection("USGS/NLCD")
 .select('landcover')
 .filterDate('2016-01-01')
@@ -118,14 +127,28 @@ var landcoverVis = {
 Map.addLayer(DC_2001, landcoverVis, 'DC_2001');
 Map.addLayer(DC_2016, landcoverVis, 'DC_2016');
 
-var DC_outline = DC_watershed.style
-({color: 'ff1a1a', 'fillColor': 'FFFFFF00', 'lineType': 'solid'});
+//Step 3. Add Deer Creek watershed layer (created in ArcGIS)
+//optional viz:
+//var DC_outline = DC_watershed.style
+//({color: 'ff1a1a', 'fillColor': 'FFFFFF00', 'lineType': 'solid'});
 
 //Map.addLayer(DC_outline);
 
-///////PART 2. FIND % EACH LC WITHIN THE WATERSHED////////
+//Step 4. Add DC_catchments layer
+//'catchments' is a Feature Collection layer created in ArcGIS containing the upstream catchment
+//from each site to the next upstream site. The column 'gridcode' indicates which
+//site the catchment corresponds to.
 
-  //Calculate DC watershed area in km2
+Map.addLayer(catchments, {color: '9c2828', 'fillColor': 'ffffff00'}, 'catchments');
+
+//step 5. Add DC sites (ArcGIS layer)
+var sites = Map.addLayer(DC_sites, {color:'ABCD6C', 'fillColor': 'ABCD6C'}, 'sites');
+
+///////Part 2. Find % each Landcover class within the watershed, 2001 and 2016////////
+
+  //Step 1. Methods demo: % within whole watershed:
+
+  //1a. Calculate DC watershed area in km2
 
     var DC_watershed_area = DC_watershed.map(function(feature){
     var num = ee.Number.parse(feature.get('AreaSqKm'));
@@ -135,25 +158,24 @@ var DC_outline = DC_watershed.style
   print('DC_area', DC_watershed_area.aggregate_stats('AreaSqKm'));
   var area = 231.7;
 
-//Reduce DC_2001 and DC_2016 to an image
+//1b. Reduce DC_2001 and DC_2016 to an image
 var DC_2001_image = DC_2001.reduce(ee.Reducer.median());
 print('DC 2001 image', DC_2001_image);
 
 var DC_2016_image = DC_2016.reduce(ee.Reducer.median());
 
-//isolate just one LC: forest=41,42,43 for NLCD. Need forest=1, all else=0
-
+//1c. Isolate just one LC: forest=41,42,43. Need forest=1, all else=0
 
   var forest_mask_2001 = DC_2001_image.eq(41).add(DC_2001_image.eq(42))
   .add(DC_2001_image.eq(43))
   .clipToCollection(DC_watershed);
      print(forest_mask_2001, {color:'000FF00'}, 'forest_mask');
 
-  //calculate the area of each forest pixel
+  //1d. calculate the area of each forest pixel
   var forest_area2001 = forest_mask_2001.multiply(ee.Image.pixelArea().divide(1000*1000));
      print('forest_area 2001', forest_area2001);
 
-  //cast result of reduce region as an image
+  //1e. cast result of reduce region as an image
      var forest_sum2001 = forest_area2001.reduceRegion({
        reducer: ee.Reducer.sum(),
        geometry: DC_watershed,
@@ -167,51 +189,49 @@ var DC_2016_image = DC_2016.reduce(ee.Reducer.median());
   print('forest cast', forest_cast);
   Map.addLayer(forest_cast, {color:'000000'}, 'forest cast');
 
-  ////now make it a number
+  ////1f. now make it a number
   var forest_num = ee.Number(forest_cast.select('landcover_median')
   .reduceRegion(ee.Reducer.first(), DC_watershed, 10)
   .get('landcover_median'));
 
   print("forest number", forest_num);
 
-  ////now get the percentage
+  ////1g. now get the percentage
   var forest_2001percent = forest_num.divide(area).multiply(100);
   print('forest 2001 percent', forest_2001percent);
 
-////////////////////// Make this a function ///////////////////////////////
-//mapped over x geometry (catchments, a Feature Collection)//
-//(ultimately, will want to run this over totalsheds layer)
+///////Part 3. Find % each Landcover class within catchments, 2001 and 2016////////
+//same method as Part 2, without percentage function
+//could probably be refined with more iteration, I ran out of time
 
-  //In catchments, convert Shape_area from a float to a Number.
-  //Also convert area from m2 to km2
+  //Step 1. Get the area of catchments in m2.
   var catchments_area = catchments.map(function(feature){
     var num = ee.Number.parse(feature.get('Shape_Area')).divide(1000*1000);
     return feature.set('Shape_Area', num);
   });
-  print('catchment areas', catchments_area);
+  print('catchment areas km2', catchments_area);
 
-//FOREST FUNCTION (2001)
-var catchments_area = catchments_area.map(function(feature){
-    var num = ee.Number.parse(feature.get('Shape_Area')).divide(1000*1000);
-    return feature.set('Shape_Area', num);
-  });
+//Step 2. FOREST FUNCTION (2001)
 
-var forest_function = catchments.map (function(x) {
+var forest_function = catchments_area.map (function(x) {
 
 var forest_mask = DC_2001_image.eq(41).add(DC_2001_image.eq(42))
-  .add(DC_2001_image.eq(43))
+  .add(DC_2001_image.eq(43))//isolate only forest landcover values
   .clipToCollection(ee.FeatureCollection(x));
 
-var area = forest_mask.multiply(ee.Image.pixelArea().divide(1000*1000));
+var area = forest_mask.multiply(ee.Image.pixelArea().divide(1000*1000));//find area of all forest pixels
 
+//reduce areas into x Feature Collection (catchments)
 var sum = area.reduceRegion({
        reducer: ee.Reducer.sum(),
        geometry: ee.FeatureCollection(x),
        scale: 30,
        maxPixels: 1e13});
 
+//cast result as an image: allows you to calc percentage later if you want
 var cast_image = sum.toImage().toFloat().clipToCollection(ee.FeatureCollection(x));
 
+//convert to number: allows you to calc percentage later if you want
 var number = ee.Number(cast_image.select('landcover_median')
  .reduceRegion(ee.Reducer.first(), ee.FeatureCollection(x), 30)
   .get('landcover_median'));
@@ -220,9 +240,9 @@ return x.set('raw forest area 2001 km2', number);
 });
 print('catchment forest results', forest_function);
 
-///////////////////////REPEAT THIS PROCESS FOR ALL OTHER LC's///////////////////////
+//Step 3. Repeat this process for all other landcover classes in 2001
 
-//WATER FUNCTION (2001)
+//3a. water (2001)
 var water_function = forest_function.map( function(x) {
 
 var water_mask = DC_2001_image.eq(11)
@@ -246,8 +266,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw water area 2001 km2', number);
 });
 
-
-//urban (21-24)
+//3b. urban (21-24)
 var urban_function = water_function.map( function(x) {
 
 var urban_mask = DC_2001_image.eq(21).add(DC_2001_image.eq(22))
@@ -273,7 +292,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw urban area 2001 km2', number);
 });
 
-//agriculture (81, 82)
+//3c. agriculture (81, 82)
 var ag_function = urban_function.map( function(x) {
 
 var ag_mask = DC_2001_image.eq(81).add(DC_2001_image.eq(82))
@@ -298,7 +317,7 @@ return x.set('raw ag area 2001 km2', number);
 });
  //0% ag//
 
-//grassland (71)
+//3d. grassland (71)
 var grassland_function = ag_function.map( function(x) {
 
 var grass_mask = DC_2001_image.eq(71)
@@ -322,7 +341,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw grassland area 2001 km2', number);
 });
 
-//scrub (51, 52)
+//3e. scrub (51, 52)
 var scrub_function = grassland_function.map( function(x) {
 
 var scrub_mask = DC_2001_image.eq(51).add(DC_2001_image.eq(52))
@@ -348,8 +367,10 @@ return x.set('raw scrub area 2001 km2', number);
 
 print('2001 results', scrub_function);
 
-/////////////////REPEAT FOR 2016//////////////////////////
+//Step 4. Repeat this process for 2016
+//use previous result as input for next function (need to iterate this)
 
+//4a. Forest
 var forest16_function = scrub_function.map (function(x) {
 
 var forest_mask = DC_2016_image.eq(41).add(DC_2016_image.eq(42))
@@ -373,9 +394,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw forest area 2016 km2', number);
 });
 
-///////////////////////REPEAT THIS PROCESS FOR ALL OTHER LC's///////////////////////
-
-//WATER FUNCTION (2001)
+//4b. water
 var water16_function = forest16_function.map( function(x) {
 
 var water_mask = DC_2016_image.eq(11)
@@ -399,8 +418,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw water area 2016 km2', number);
 });
 
-
-//urban (21-24)
+//4c. urban (21-24)
 var urban16_function = water16_function.map( function(x) {
 
 var urban_mask = DC_2016_image.eq(21).add(DC_2016_image.eq(22))
@@ -426,7 +444,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw urban area 2016 km2', number);
 });
 
-//agriculture (81, 82)
+//4d. agriculture (81, 82)
 var ag16_function = urban16_function.map( function(x) {
 
 var ag_mask = DC_2016_image.eq(81).add(DC_2016_image.eq(82))
@@ -449,9 +467,8 @@ var number = ee.Number(cast_image.select('landcover_median')
 
 return x.set('raw ag area 2016 km2', number);
 });
- //0% ag//
 
-//grassland (71)
+//4f. grassland (71)
 var grassland16_function = ag16_function.map( function(x) {
 
 var grass_mask = DC_2016_image.eq(71)
@@ -475,7 +492,7 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw grassland area 2016 km2', number);
 });
 
-//scrub (51, 52)
+//4g. scrub (51, 52)
 var scrub16_function = grassland16_function.map( function(x) {
 
 var scrub_mask = DC_2016_image.eq(51).add(DC_2016_image.eq(52))
@@ -499,8 +516,8 @@ var number = ee.Number(cast_image.select('landcover_median')
 return x.set('raw scrub area 2016 km2', number);
 });
 
-////////////Export final feature collection to drive/////////////////
-Export.table.toDrive({
- collection: scrub16_function,
- description: 'catchment results'
-});
+////////////Part 4. Export final feature collection to drive///////////////
+//Export.table.toDrive({
+//collection: scrub16_function,
+//description: 'catchment results'
+//});
